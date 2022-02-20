9f3e4eb1ba6bcb22778c660b2fdba493d8ab718d
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-02-20 14:16:38 -0300

    feat: add function to shuffle color orders

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+let order = [];
+let clickOrder = [];
+let score = 0;
+
+// Ordem: 0 - verde; 1 - vermelho; 2 - amarelo; 3 - azul
+
+const blue = document.querySelector('.color-blue')
+const yellow = document.querySelector('.color-yellow')
+const green = document.querySelector('.color-green')
+const red = document.querySelector('.color-red')
+
+const lightColor = (element, number) => {
+  number *= 500;
+  setTimeout(() => {
+    element.classList.add('selected')
+  }, number - 250);
+  setTimeout(() => {
+    element.classList.remove('selected')
+  });
+};
+
+const shuffleOrder = () => {
+  let colorOrder = Math.floor(Math.random() * 4);
+  order[order.length] = colorOrder;
+  clickedOrder = [];
+  
+  for (let i in order) {
+    let elementColor = createColorElement(order[i]);
+    lightColor(elementColor, Number(i) + 1);
+  }; 
+};
+
